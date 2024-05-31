aebe785861c2ecff7fab0fdc27c04cdf02f3cbb7
boobi <mehboobarshad300@gmail.com>
2024-05-31 13:11:24 +0000

    changes added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,7 +11,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/img.jpg";
+import image from "../images/deso.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
