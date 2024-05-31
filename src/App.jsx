8275348466cc7a6b1e7d097301b9fd743691ffcb
Mehboob Arshad <130151311+mehboob14@users.noreply.github.com>
2024-05-31 11:26:00 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Mehboob",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mehboob Arshad",
+  title: "Software Developer & UserInterface Designer",
+  email: "mehboob14@proton.me,
+  gitHub: "https:github.com/mehboob14",
+  instagram: "mehboob",
+  linkedIn: "https://linkdin/in/mehboob-arshad",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "mehboob",
+  youTube: "BubbTech",
 };
 
 const primaryColor = "#4E567E";
